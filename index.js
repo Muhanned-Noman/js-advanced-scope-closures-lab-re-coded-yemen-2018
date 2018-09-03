d1590e4cc3aab9992fcf594b3172718e7ee30011
Muhanned Abdulhaq Mohammed Noman <muhanned.noman@gmail.com>
2018-09-03 19:05:41 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+function produceDrivingRange(blockRange){
+  return function(firstValue, secondValue){
+    let result = parseInt(secondValue) - parseInt(firstValue);
+
+    if(blockRange < result){
+      return 'within range by ${result}';
+    }else{
+      return '${result} blocks out of range';
+    }
+  }
+}
